a63a88b89390f3be831123193b0d53952339285e
Zach Taylor <ztayut2tx@gmail.com>
2021-03-23 13:48:42 -0500

    created footer and Navbar components

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return (
+        <footer style={{ textAlign: "center"}}>
+            <div className="container">
+                <p id="footer">Zach Taylor &copy; 2021</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ portfolio/src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{ backgroundColor: "rgb(42, 127, 212)" }}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Zach</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/portfolio">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
