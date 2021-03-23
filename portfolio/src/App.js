c9ca6e2efaa25170f47a7b112791928d38e3ff1e
Zach Taylor <ztayut2tx@gmail.com>
2021-03-23 13:30:24 -0500

    imported components into app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import About from './pages/About';
+import Nav from './components/Nav';
+import Portfolio from './pages/Portfolio';
+import Footer from './components/Footer';
+import Contact from './pages/Contact';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <div className="content-wrap">
+      <Router basename="/react-portfolio">
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+      </Router>
+      </div>
+      <Footer />
     </div>
   );
 }
